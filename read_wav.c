27526841d112d82d48ff66aadf89ef39a5d408f9
Matt MacLeod <Matt@Matts-MBP.hitronhub.home>
2021-07-25 14:13:14 -0700

    Forgot Decompress func

--- read_wav.c
+++ read_wav.c
@@ -166,7 +166,7 @@ int encode_wave_file(char* input_file_name, char* output_file_name, bool encodeT
 
     if (encodeType == DECOMPRESS){ 
 
-output_file_data_buffer = malloc((overall_size) * sizeof(char));
+        output_file_data_buffer = malloc((overall_size) * sizeof(char));
 
 
         for (i = 0; i < overall_size; i = i + 8) {
@@ -176,10 +176,10 @@ output_file_data_buffer = malloc((overall_size) * sizeof(char));
             input_data3 = bytes_to_int16(inputfile_data_buffer[i + 4], inputfile_data_buffer[i + 5]);
             input_data4 = bytes_to_int16(inputfile_data_buffer[i + 6], inputfile_data_buffer[i + 7]);
             
-            codeword1 = codeword_compression(input_data1,signum(input_data1));
-            codeword2 = codeword_compression(input_data2,signum(input_data2));
-            codeword3 = codeword_compression(input_data3,signum(input_data3));
-            codeword4 = codeword_compression(input_data4,signum(input_data4));
+            codeword1 = codeword_decompression(input_data1);
+            codeword2 = codeword_decompression(input_data2);
+            codeword3 = codeword_decompression(input_data3);
+            codeword4 = codeword_decompression(input_data4);
          
             
             output_file_data_buffer[i / 2] = codeword1;
