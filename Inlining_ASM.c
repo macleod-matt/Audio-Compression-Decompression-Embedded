4507ca89d09b8ca2060cd276f390cb3b83366b22
CalderStaude <35546694+CalderStaude@users.noreply.github.com>
2021-08-11 12:03:09 -0700

    Updated code to work for inlining

--- Inlining_ASM.c
+++ Inlining_ASM.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
+
+
+void ASMInlined(int Val2, int Val3)
+{
+        int sum = Val2 + Val3;
+        return;
+}
+
+
 int main(void)
 {
     printf("Testing1234");
-    int Val3 = 1;
+    int Val1 = 1;
     int Val2 = 2;
     int Val3 = 3;
-    __asm__("ASMInlined   %0, %1, %2":"=r"(Val1):"r"(Val2),"r"(Val3));;
+    __asm__("ASMInlined   %0, %1, %2":"=r"(Val1):"r"(Val2),"r"(Val3));
     int sum = Val2 + Val3;
     printf("Inline Worked");
+
+        return;
 }
