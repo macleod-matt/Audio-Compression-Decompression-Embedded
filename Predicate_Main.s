0b3f195c2ec54da4155c438ef0aa159f2b5dddbc
Calder Staude <calderstaude@seng440.ece.uvic.ca>
2021-08-12 17:45:49 -0700

    ASM for Predicate Define Operation

---
+++ Predicate_Main.s
@@ -0,0 +1,28 @@
+	.arch armv4t
+	.fpu softvfp
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 6
+	.eabi_attribute 18, 4
+	.file	"Predicate_Main.c"
+	.text
+	.align	2
+	.global	main
+	.type	main, %function
+main:
+	@ Function supports interworking.
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 1, uses_anonymous_args = 0
+	@ link register save eliminated.
+	str	fp, [sp, #-4]!
+	add	fp, sp, #0
+	add	sp, fp, #0
+	ldmfd	sp!, {fp}
+	bx	lr
+	.size	main, .-main
+	.ident	"GCC: (Sourcery G++ Lite 2008q3-72) 4.3.2"
+	.section	.note.GNU-stack,"",%progbits
