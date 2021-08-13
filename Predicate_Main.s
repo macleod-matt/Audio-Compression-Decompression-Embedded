661fb2b629b82343a7192aad1ace6f747936161e
Calder Staude <calderstaude@seng440.ece.uvic.ca>
2021-08-12 18:04:27 -0700

    Added program ASM and MuLawAPI ASM

--- Predicate_Main.s
+++ Predicate_Main.s
@@ -9,6 +9,10 @@
 	.eabi_attribute 30, 6
 	.eabi_attribute 18, 4
 	.file	"Predicate_Main.c"
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"%d\000"
 	.text
 	.align	2
 	.global	main
@@ -17,12 +21,21 @@ main:
 	@ Function supports interworking.
 	@ args = 0, pretend = 0, frame = 0
 	@ frame_needed = 1, uses_anonymous_args = 0
-	@ link register save eliminated.
-	str	fp, [sp, #-4]!
-	add	fp, sp, #0
-	add	sp, fp, #0
-	ldmfd	sp!, {fp}
+	stmfd	sp!, {fp, lr}
+	add	fp, sp, #4
+	ldr	r0, .L3
+	mov	r1, #5120
+	add	r1, r1, #52
+	bl	printf
+	mov	r3, #0
+	mov	r0, r3
+	sub	sp, fp, #4
+	ldmfd	sp!, {fp, lr}
 	bx	lr
+.L4:
+	.align	2
+.L3:
+	.word	.LC0
 	.size	main, .-main
 	.ident	"GCC: (Sourcery G++ Lite 2008q3-72) 4.3.2"
 	.section	.note.GNU-stack,"",%progbits
